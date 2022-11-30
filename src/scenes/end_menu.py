895073dea3b20e3976ea3a655bafb774ccdf705f
DaNub <75505891+DaNubCoding@users.noreply.github.com>
2022-11-29 21:50:09 -0500

    end menu transitions (all transitions done!!)

--- src/scenes/end_menu.py
+++ src/scenes/end_menu.py
@@ -1,19 +1,18 @@
 from pygame.locals import KEYDOWN, K_SPACE
+import pytweening as tween
 import pygame
 
-from src.common.constants import HIGHSCORE_FILE, CENTER, BOLD_FONTS, Anchors
+from src.common.constants import HIGHSCORE_FILE, CENTER, BOLD_FONTS, Anchors, MIN_BLUR_THRESHOLD
 from src.gui.elements import Label, Image
 from src.management.scene import Scene
 from src.common.utils import blur_surf
+from src.common.tween import Tween
 
-class EndMenu(Scene):
+class EndMenuGUI(Scene):
     def setup(self) -> None:
         super().setup()
 
-        self.volume = pygame.mixer.music.get_volume()
-        pygame.mixer.fadeout(500)
-
-        Image(self, (0, 0), blur_surf(self.previous_scene.surface), anchor=Anchors.TOPLEFT)
+        self.surface = self.surface.convert_alpha()
 
         try: # Try to open the highscore file and read the highscore
             with open(HIGHSCORE_FILE, "r") as f:
@@ -37,8 +36,40 @@ class EndMenu(Scene):
         Label(self, CENTER - (0, 100), f"Score: {self.previous_scene.player.score}", BOLD_FONTS[64], (230, 230, 230))
         Label(self, CENTER + (0, 100), "Press space to restart", BOLD_FONTS[18], (230, 230, 230))
 
+        self.alpha_tween = Tween(self, 0, 255, 500, tween.easeInSine)
+
     def update(self) -> None:
         super().update()
+
+        self.alpha_tween()
+
+    def pre_sprite(self) -> None:
+        self.surface.fill((0, 0, 0, 0))
+
+    def post_sprite(self) -> None:
+        if self.alpha_tween.ended: return
+        self.surface.set_alpha(self.alpha_tween.value)
+
+class EndMenu(Scene):
+    def setup(self) -> None:
+        super().setup()
+
+        self.volume = pygame.mixer.music.get_volume()
+        pygame.mixer.fadeout(500)
+
+        self.bg_img = Image(self, (0, 0), self.previous_scene.surface, anchor=Anchors.TOPLEFT)
+
+        self.gui = EndMenuGUI(self.manager, self.previous_scene, self)
+        self.gui.setup()
+
+        self.blur_tween = Tween(self, 0.25, MIN_BLUR_THRESHOLD, -0.8, tween.easeInOutQuad)
+        self.blur_tween.reset()
+        self.ending = False
+
+    def update(self) -> None:
+        super().update()
+
+        self.gui.update()
 
         if self.volume > 0:
             self.volume -= 0.05 * self.dt
@@ -47,6 +78,19 @@ class EndMenu(Scene):
             self.volume = 0
             pygame.mixer.music.pause()
 
+        self.blur_tween()
+        if not self.blur_tween.ended:
+            self.bg_img.surface = blur_surf(self.previous_scene.surface, self.blur_tween.value)
+
         if KEYDOWN in self.manager.events and self.manager.events[KEYDOWN].key == K_SPACE:
-            # Start new game
+            self.blur_tween = Tween(self, 0.03, 0.25, -0.2, tween.easeInExpo, cutoff=0.031)
+            self.blur_tween.reset()
+            self.gui.alpha_tween = Tween(self, 0, 255, -200, tween.easeInSine)
+            self.gui.alpha_tween.reset()
+            self.ending = True
+
+        if self.ending and self.blur_tween.ended:
             self.manager.new_scene("MainGame")
+
+    def post_sprite(self) -> None:
+        self.gui.draw()
